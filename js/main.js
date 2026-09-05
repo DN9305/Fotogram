b95649debbc144277c87246dce5c07c65ff6ea68
David Dev <david.ebou@gmx.de>
2026-09-05 20:57:39 +0200

    update

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function addPhotosToGallery() {
     let galleryElems = "";
 
     for (let i = 0; i < IMAGE_CONTAINER.length; i++) {
-        galleryElems += `<img id="img_${i}" class="" src="${IMAGE_CONTAINER[i].path}" alt="${IMAGE_CONTAINER[i].alt}" onclick="openDialog(${i})">`
+        galleryElems += `<img id="img_${i}" tabindex="0" src="${IMAGE_CONTAINER[i].path}" alt="${IMAGE_CONTAINER[i].alt}" onclick="openDialog(${i})" onkeypress='if(event.key=="Enter"){openDialog(${i})}' aria-label="opens Dialog with the same Picture scaled">`
     };
 
     GALLERY.innerHTML = galleryElems;
@@ -31,22 +31,9 @@ function addPhotosToGallery() {
 
 function openDialog(index) {
 
-    let lastSlash = IMAGE_CONTAINER[index].path.lastIndexOf("/")
-    let lastPoint = IMAGE_CONTAINER[index].path.lastIndexOf(".")
-    let headlineName = IMAGE_CONTAINER[index].path.slice(lastSlash+1, lastPoint)
-
     DIALOG.showModal()
-    DIALOG_IMG.innerHTML = /*html*/`
-        <img id="dialog-img_${index}"src="${IMAGE_CONTAINER[index].path}" alt="${IMAGE_CONTAINER[index].alt}"> `
-    DIALOG_SPAN.innerHTML = /*html*/`
-        ${index + 1}/${IMAGE_CONTAINER.length}
-    `
-    DIALOG_HEADLINE.innerHTML = headlineName
-
-    DIALOG_BUTTON_CLOSE.onclick = function () {
-        DIALOG.close()
-    }
-
+    headline(index)
+    innerDialogHtml(index)
     DIALOG_BUTTON_LEFT.onclick = function () {
         if (index === 0) {
             openDialog(IMAGE_CONTAINER.length - 1)
@@ -55,12 +42,36 @@ function openDialog(index) {
         }
     }
     DIALOG_BUTTON_RIGHT.onclick = function () {
-        if (index === 11) {
+        if (index === IMAGE_CONTAINER.length-1) { 
             openDialog(0)
         } else {
             openDialog(index + 1)
         }
     }
+}
+
+
+function innerDialogHtml(index) {
+    DIALOG_IMG.innerHTML = /*html*/`
+        <img id="dialog-img_${index}"src="${IMAGE_CONTAINER[index].path}" alt="${IMAGE_CONTAINER[index].alt}"> `
+    DIALOG_SPAN.innerHTML = /*html*/`
+        ${index + 1}/${IMAGE_CONTAINER.length}  `
+}
+
+function headline(index) {
+    let lastSlash = IMAGE_CONTAINER[index].path.lastIndexOf("/")
+    let lastPoint = IMAGE_CONTAINER[index].path.lastIndexOf(".")
+    let headlineName = IMAGE_CONTAINER[index].path.slice(lastSlash + 1, lastPoint)
+    DIALOG_HEADLINE.innerHTML = headlineName
+}
+
+/////////////////////////////
+//Mark: BUTTON ONCLICK 
+/////////////////////////////
+
+
+DIALOG_BUTTON_CLOSE.onclick = function () {
+    DIALOG.close()
 }
 
 //////////////////////////////////
