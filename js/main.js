70f499e3329fc7837d28dafabd38973c5a1ca022
David Dev <david.ebou@gmx.de>
2026-09-05 19:03:33 +0200

    update

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const DIALOG_IMG = document.getElementById("dialog-img")
 const DIALOG_SPAN = document.getElementById("dialog-span")
 const DIALOG_BUTTON_LEFT = document.getElementById("button-left")
 const DIALOG_BUTTON_RIGHT = document.getElementById("button-right")
+const DIALOG_BUTTON_CLOSE = document.getElementById("dialog-header-button")
+const DIALOG_HEADLINE = document.getElementById("dialog-headline")
 
 ///////////////////////////////////
 //Mark: PHOTO GALLERY
@@ -28,12 +30,23 @@ function addPhotosToGallery() {
 /////////////////////////////////
 
 function openDialog(index) {
+
+    let lastSlash = IMAGE_CONTAINER[index].path.lastIndexOf("/")
+    let lastPoint = IMAGE_CONTAINER[index].path.lastIndexOf(".")
+    let headlineName = IMAGE_CONTAINER[index].path.slice(lastSlash+1, lastPoint)
+
     DIALOG.showModal()
     DIALOG_IMG.innerHTML = /*html*/`
         <img id="dialog-img_${index}"src="${IMAGE_CONTAINER[index].path}" alt="${IMAGE_CONTAINER[index].alt}"> `
     DIALOG_SPAN.innerHTML = /*html*/`
         ${index + 1}/${IMAGE_CONTAINER.length}
     `
+    DIALOG_HEADLINE.innerHTML = headlineName
+
+    DIALOG_BUTTON_CLOSE.onclick = function () {
+        DIALOG.close()
+    }
+
     DIALOG_BUTTON_LEFT.onclick = function () {
         if (index === 0) {
             openDialog(IMAGE_CONTAINER.length - 1)
